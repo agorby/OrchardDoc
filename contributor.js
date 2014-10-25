0e22d85b42eb0fa0383c9063028ad0cd5f4ce32c
Kindly Man <aslanovm@gmail.com>
2014-10-26 01:31:20 +0300

    Продолжаем адаптировать под русский сайт

--- contributor.js
+++ contributor.js
@@ -1,5 +1,5 @@
 function OrchardContributor($scope, $http) {
-    $http.get('https://api.github.com/repos/OrchardCMS/OrchardDoc/contributors').
+    $http.get('https://api.github.com/repos/KindlySoul/OrchardDoc/contributors').
         success(function(data) {
             $scope.totalContribtors = data;
         });
